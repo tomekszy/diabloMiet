d9f1b6b7d73de884680a34ed901d7cec0a7446d0
Mariusz Zapisek <mariusz.zapisek@getresponse.com>
2019-02-08 22:07:43 +0100

    img

---
+++ Dungeon.js
@@ -0,0 +1,42 @@
+function Dungeon() {
+
+    this.tileHeight = 128;
+    this.tileWidth = 64;
+
+    this.mapWidth = 16;
+    this.mapHeight = 10;
+
+    this.map = [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []
+    ];
+
+    this.tiles = {
+        'floorTileBlood1': {
+            x: ,
+            y:  
+        },
+        'floorTileBasic1': {
+
+        },
+        'floorTileBasic2': {
+
+        },
+        'floorTileCircle1': {
+
+        },
+        'floorTileCircle2': {
+
+        }
+    };
+
+
+}
